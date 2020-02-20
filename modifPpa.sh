924f20cb95b81358ad01157642b76baa8a68399b
omohammed95 <53604473+omohammed95@users.noreply.github.com>
2020-02-20 15:01:28 +0100

    ...

--- modifPpa.sh
+++ modifPpa.sh
@@ -2,9 +2,11 @@
 
 function DISABLE_PPAs {
         #this function will add the # character in lines that begin with deb.
-	for i in ((docker-ce.list slack-desktop.list user.list))
+	for i in docker-ce.list slack-desktop.list user.list
+	do
         sed -i -E 's/^/#/' /etc/apt/sources.list.d/$i
-
+	echo "files has been modified successfully"
+	done
 
 
 }
