f313f12d7c2dbd3250e722ce6948cabd267403d1
omohammed95 <53604473+omohammed95@users.noreply.github.com>
2020-02-20 15:19:06 +0100

    correction

--- configfile.sh
+++ configfile.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
 function SWITCH_TO_LTS {
-        #this function will check if  the  prompt is set as lts if not it will switch it to lts 
-        if [ "$SWITCH_PROMPT_TO_LTS" = "1" ]; then
-
-          echo "Switching to the 'lts' release channel (if 'never' or 'normal')"
-          sed -i -E 's/Prompt=(never|normal)/Prompt=lts/g' "/etc/update-manager/release-upgrades"
-
+        #this function will check if  the  prompt is set as lts if not it will switch it to lts.
+        TEST=cat | grep "Prompt=lts" /etc/update-manager/release-upgrades 
+        if [ "$TEST" = "0" ]; then
+           echo "prompt is already set in lts"
         else
+           echo "Switching to the 'lts' release channel (if 'never' or 'normal')"
+           sed -i -E 's/Prompt=(never|normal)/Prompt=lts/g' "/etc/update-manager/release-upgrades"
 
-          echo "prompt is already set in lts"
 
         fi
-}
 
+}
+        
 function DISABLE_PPAs {
         #this function will add the # character in lines that begin with deb.
-        sed -i -E 's/^/#/' /etc/apt/sources.list.d/*.list
-
+	for i in docker-ce.list slack-desktop.list user.list
+	do
+        sed -i -E 's/^/#/' /etc/apt/sources.list.d/$i
+	echo "files has been modified successfully"
+	done
 
 
 }
