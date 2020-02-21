cecdff317721f489ccc592e579797099f7dafd49
omohammed95 <53604473+omohammed95@users.noreply.github.com>
2020-02-21 11:01:58 +0100

    Update switch.sh

--- switch.sh
+++ switch.sh
@@ -6,7 +6,7 @@ cd /etc/update-manager/
 File=release-upgrades  
 if grep -q Prompt=lts "$File"; 
 then
-echo "nothing to it's already in lts"
+echo "nothing to do it's already in lts"
 else
  echo "Switching to the 'lts' release channel (if 'never' or 'normal')"
  sed -i -E 's/Prompt=(never|normal)/Prompt=lts/g' "/etc/update-manager/release-upgrades"
