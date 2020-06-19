2969e217849b430e66d8bc0005503e6534fc3e85
omohammed95 <osman.mohammed@camptocamp.com>
2020-06-19 10:00:14 +0200

    update1_c2c.sh added

---
+++ update1_c2c.sh
@@ -0,0 +1,139 @@
+#!/bin/bash  -x
+
+function LogRight {
+
+        # Checking if the user has run the script with "sudo" or not
+       if [[ $EUID -ne 0 ]] ; then
+        clear
+        echo ""
+        echo "c2c upgrade tool must be run as root or with sudo. Now I will just exit..." 1>&2
+        echo ""
+        sleep 2
+        exit 1
+       fi
+}
+
+function setup {
+
+  	# check to install the needed  dependencies
+  	#install deborphan
+  	apt install deborphan
+
+}
+
+greeting() {
+
+  	echo
+  	echo "Hello,Let's update this system."
+  	echo "##############################################################################################################################################################"
+  	echo "  ______    ______    ______         __    __  _______    ______   _______    ______   _______   ________        ________  ______    ______   __               "
+  	echo "       /      \  /      \  /      \       /  |  /  |/       \  /      \ /       \  /      \ /       \ /        |      /        |/      \  /      \ /  |        "
+  	echo "    /$$$$$$  |/$$$$$$  |/$$$$$$  |      $$ |  $$ |$$$$$$$  |/$$$$$$  |$$$$$$$  |/$$$$$$  |$$$$$$$  |$$$$$$$$/       $$$$$$$$//$$$$$$  |/$$$$$$  |$$ |          "
+  	echo "    $$ |  $$/ $$____$$ |$$ |  $$/       $$ |  $$ |$$ |__$$ |$$ | _$$/ $$ |__$$ |$$ |__$$ |$$ |  $$ |$$ |__             $$ |  $$ |  $$ |$$ |  $$ |$$ |          "
+  	echo "    $$ |       /    $$/ $$ |            $$ |  $$ |$$    $$/ $$ |/    |$$    $$< $$    $$ |$$ |  $$ |$$    |            $$ |  $$ |  $$ |$$ |  $$ |$$ |          "
+  	echo "    $$ |   __ /$$$$$$/  $$ |   __       $$ |  $$ |$$$$$$$/  $$ |$$$$ |$$$$$$$  |$$$$$$$$ |$$ |  $$ |$$$$$/             $$ |  $$ |  $$ |$$ |  $$ |$$ |          "
+  	echo "    $$ \__/  |$$ |_____ $$ \__/  |      $$ \__$$ |$$ |      $$ \__$$ |$$ |  $$ |$$ |  $$ |$$ |__$$ |$$ |_____          $$ |  $$ \__$$ |$$ \__$$ |$$ |_____     "
+  	echo "    $$    $$/ $$       |$$    $$/       $$    $$/ $$ |      $$    $$/ $$ |  $$ |$$ |  $$ |$$    $$/ $$       |         $$ |  $$    $$/ $$    $$/ $$       |    "
+  	echo "     $$$$$$/  $$$$$$$$/  $$$$$$/         $$$$$$/  $$/        $$$$$$/  $$/   $$/ $$/   $$/ $$$$$$$/  $$$$$$$$/          $$/    $$$$$$/   $$$$$$/  $$$$$$$$/     " 
+  	echo "###############################################################################################################################################################"
+  	echo
+
+}
+
+function DISABLE_PUPPET {
+        # this function will check ifpuppet agent disabled in the system or not
+	FILE=/opt/puppetlabs/puppet/cache/state/agent_disabled.lock
+       if [ -f "$FILE" ]; then
+          echo "puppet agent has already disabled"
+       else
+         puppet agent --disable "puppet will be disabled because the system is upgrading to new release"
+	 echo "Puppet has been diabled successfully!!  "
+       fi
+
+}
+
+function SWITCH_TO_LTS {
+        #this function will check if  the  prompt is set as lts if not it will switch it to lts.
+        TEST=cat | grep "Prompt=lts" /etc/update-manager/release-upgrades 
+        if [ "$TEST" = "0" ]; then
+           echo "prompt is already set in lts"
+        else
+           echo "Switching to the 'lts' release channel (if 'never' or 'normal')"
+           sed -i -E 's/Prompt=(never|normal)/Prompt=lts/g' "/etc/update-manager/release-upgrades"
+
+
+        fi
+
+}
+
+function DISABLE_PPAs {
+        #this function will add the # character in lines that begin with deb.
+	for i in docker-ce.list slack-desktop.list user.list
+	do
+        sed -i -E 's/^/#/' /etc/apt/sources.list.d/$i
+	echo "files has been modified successfully"
+	done
+
+
+}
+
+
+function UPDATE_C2C {
+        echo
+        echo
+        echo "#########################"
+        echo "          Started"
+        echo "#########################"
+        echo
+
+        ## Updates package lists
+        apt update;
+        echo
+        echo "###############################"
+        echo "Finished updating package lists"
+        echo "###############################"
+        sleep 1
+## Updates packages and libraries
+        apt full-upgrade;
+        echo
+        echo "###############################################"
+        echo "Finished updating packages and system libraries"
+        echo "###############################################"
+        sleep 1
+        echo
+
+        ## Removes unneeded packages
+        apt --purge autoremove;
+        echo
+        echo "###################################"
+        echo "Finished removing unneeded packages"
+        echo "###################################"
+        sleep 1
+        echo
+        ## Removes unused config files
+        deborphan -n --find-config | xargs apt --purge autoremove;
+        echo
+        echo "#####################################"
+        echo "Finished removing unused config files"
+        echo "#####################################"
+        sleep 1
+        echo
+        ## Removes package files that can no longer be downloaded and everything except
+        # the lock file in /var/cache/apt/archives, including directories.
+        apt autoclean; apt clean;
+        echo
+        echo "######################################"
+        echo " Cleaned downloaded temporary packages"
+        echo "######################################"
+        echo
+
+}
+
+
+LogRight
+setup
+greeting
+DISABLE_PUPPET
+SWITCH_TO_LTS
+DISABLE_PPAs
+UPDATE_C2C
