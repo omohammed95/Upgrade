93e64a3ef6c0e97f7fb8d07bcca670b69e89dd78
omohammed95 <osman.mohammed@camptocamp.com>
2020-06-30 10:19:14 +0200

    comment

--- version_upgrade.sh
+++ version_upgrade.sh
@@ -5,5 +5,5 @@ function UPGRADE{
 	#Tthis function will upgrade your system to new release
 	# make sure that the release upgrade option is LTS for the new disturbution release  
 	do-release-upgrade -d
-       echo "have a fun with your new release!!!"
+       echo "have a fun with your new release !!!"
 }
